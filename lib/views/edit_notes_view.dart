ae2ab76449d682e0673dbc5411134fd9a2daafce
EsraaAbdalla <essraabdalla2@gmail.com>
2023-06-15 18:24:05 +0300

    edit note done

--- lib/views/edit_notes_view.dart
+++ lib/views/edit_notes_view.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/models/note_model.dart';
 
 import 'package:notes_app/views/widgets/edit_view_body.dart';
 
 class EditNotesView extends StatelessWidget {
-  const EditNotesView({super.key});
+  const EditNotesView({super.key, required this.note});
+
+  final NoteModel note;
 
   @override
   Widget build(BuildContext context) {
     ThemeData(brightness: Brightness.dark);
-    return const Scaffold(
-      body: EditNoteViewBody(),
+    return Scaffold(
+      body: EditNoteViewBody(
+        note: note,
+      ),
     );
   }
 }
